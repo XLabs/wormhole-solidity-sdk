45343deb235013894424b1c0eec48c6ac45de3cc
Sebastián Claudio Nale <sebinale@gmail.com>
2024-08-08 19:08:51 -0300

    feat: adds `keccak256` primitive without memory allocation
    Co-authored-by: agus <agustinmaurasvelez@gmail.com>

--- src/Utils.sol
+++ src/Utils.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: Apache 2
 pragma solidity ^0.8.19;
 
@@ -15,4 +14,29 @@ function fromUniversalAddress(bytes32 universalAddr) pure returns (address addr)
     assembly ("memory-safe") {
         addr := universalAddr
     }
+}
+
+/**
+ * @dev The Solidity compiler reserves 64 bytes at the start of the memory space as "scratch space".
+ * Applications can use this scratch space for short term allocations.
+ * See https://docs.soliditylang.org/en/v0.8.25/assembly.html#memory-management
+ */
+uint256 constant scratchSpacePtr = 0x00;
+/**
+ * @dev We call word the typical unit size for operands of EVM instructions: 32 bytes.
+ */
+uint256 constant wordSize = 32;
+
+/**
+ * Hashes a 32 byte word with keccak256.
+ * @param word Word to be hashed.
+ */
+function keccak256Word(
+  bytes32 word
+) pure returns (bytes32 hash) {
+  /// @solidity memory-safe-assembly
+  assembly {
+    mstore(scratchSpacePtr, word)
+    hash := keccak256(scratchSpacePtr, wordSize)
+  }
 }
